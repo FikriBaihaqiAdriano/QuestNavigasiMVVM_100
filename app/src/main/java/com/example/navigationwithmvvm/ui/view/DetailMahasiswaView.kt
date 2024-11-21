30e809aa595e9b5d045a01a1be3adb452d54c5cb
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-11-21 21:28:54 +0700

    Membuat daftar data mahasiswa dengan tipe List

--- app/src/main/java/com/example/navigationwithmvvm/ui/view/DetailMahasiswaView.kt
+++ app/src/main/java/com/example/navigationwithmvvm/ui/view/DetailMahasiswaView.kt
@@ -1,27 +1,38 @@
 package com.example.navigationwithmvvm.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.example.navigationwithmvvm.model.DataMahasiswa
 
 @Composable
-fun DetailMahasisawaView(
+fun DetailMahasiswaView(
     modifier: Modifier = Modifier,
     uiStateMahasiswa: DataMahasiswa,
+    navHostController: NavHostController
 ){
-    val listDataMhs = listOf(
+    val listDataMahasiswa = listOf(
         Pair("Nama", uiStateMahasiswa.nama),
         Pair("Gender", uiStateMahasiswa.gender),
         Pair("Alamat", uiStateMahasiswa.alamat),
     )
 
-    Column() {
-        listDataMhs.forEach {
-            CardSection(
-                judulParam = items.first,
-                isiParam = items.second
-            )
-        }
-    }
+}
+
+@Composable
+fun CardSection(Judul:String, IsiJudul:String){
+
 }
