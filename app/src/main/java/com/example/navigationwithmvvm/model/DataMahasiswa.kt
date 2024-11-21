ee41c0ea3e7708b1417e6874838f265b8425cb30
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-11-21 21:59:23 +0700

    menambah nim dalam DataMahasiswa

--- app/src/main/java/com/example/navigationwithmvvm/model/DataMahasiswa.kt
+++ app/src/main/java/com/example/navigationwithmvvm/model/DataMahasiswa.kt
@@ -2,6 +2,7 @@ package com.example.navigationwithmvvm.model
 
 data class DataMahasiswa(
     val nama: String = "",
+    val nim: String ="",
     val gender: String = "",
     val alamat: String = "",
 )
